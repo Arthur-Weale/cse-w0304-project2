3d621276a3991db7c96fb617cf4660c15efc1580
Arthur Weale <ajayrossy340@gmail.com>
2025-05-28 14:50:08 +0200

    Fixed  everything, assignment complete

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,47 +6,21 @@ const validate = require('../middleware/validate');
 // GET all books
 router.get('/', 
   // #swagger.tags = ['Books']
-  // #swagger.summary = 'Get all books'
-  // #swagger.path = '/api/books'
+   // #swagger.path = '/api/books'
   controller.getAll
 );
 
 // GET a book by ID
 router.get('/:id', 
   /*
-    #swagger.tags = ['Books']
-    #swagger.summary = 'Get a book by ID'
-    #swagger.path = '/api/books/{id}'
-    #swagger.parameters['id'] = {
-      in: 'path',
-      description: 'Book ID',
-      required: true,
-      type: 'string'
-    }
-  */
+    #swagger.tags = ['Books']*/
   controller.getOne
 );
 
 // POST create a book
 router.post('/', 
   // #swagger.tags = ['Books']
-  // #swagger.summary = 'Create a new book'
   // #swagger.path = '/api/books'
-  /*
-    #swagger.parameters['body'] = {
-      in: 'body',
-      description: 'Book data',
-      required: true,
-      schema: {
-        type: 'object',
-        properties: {
-          title: { example: 'any' },
-          authorId: { example: 'any' },
-          isbn: { example: 'any' }
-        }
-      }
-    }
-  */
   validate.book, 
   controller.create
 );
@@ -54,29 +28,6 @@ router.post('/',
 // PUT update a book
 router.put('/:id', 
   // #swagger.tags = ['Books']
-  // #swagger.summary = 'Update a book'
-  // #swagger.path = '/api/books/{id}'
-  /*
-    #swagger.parameters['id'] = {
-      in: 'path',
-      description: 'Book ID',
-      required: true,
-      type: 'string'
-    }
-    #swagger.parameters['body'] = {
-      in: 'body',
-      description: 'Book data to update',
-      required: true,
-      schema: {
-        type: 'object',
-        properties: {
-          title: { example: 'any' },
-          authorId: { example: 'any' },
-          isbn: { example: 'any' }
-        }
-      }
-    }
-  */
   validate.book, 
   controller.update
 );
@@ -84,17 +35,6 @@ router.put('/:id',
 // DELETE a book
 router.delete('/:id', 
   // #swagger.tags = ['Books']
-  // #swagger.summary = 'Delete a book'
-  // #swagger.path = '/api/books/{id}'
-  /*
-    #swagger.parameters['id'] = {
-      in: 'path',
-      description: 'Book ID',
-      required: true,
-      type: 'string',
-      example: '6835f50ec0132b48c535f444'
-    }
-  */
   controller.remove
 );
 
