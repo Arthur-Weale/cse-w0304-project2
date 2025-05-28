066521665cdcd33dc73ca8dff41a9c1a6ca02ad2
Arthur Weale <ajayrossy340@gmail.com>
2025-05-28 16:39:24 +0200

    Swagger Ui fixed

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,7 +1,8 @@
 const Author = require('../models/author');
 
+// GET all authors
 exports.getAll = async (req, res) => {
-  //#swagger.tags=["Author"]
+  // #swagger.tags = ["Author"]
   try {
     const authors = await Author.find();
     res.status(200).json(authors);
@@ -10,12 +11,62 @@ exports.getAll = async (req, res) => {
   }
 };
 
+// GET one author by ID
+exports.getOne = async (req, res) => {
+  // #swagger.tags = ["Author"]
+  // #swagger.summary = "Get author by ID"
+  // #swagger.parameters['id'] = { in: 'path', description: 'Author ID', required: true, type: 'string' }
+  try {
+    const author = await Author.findById(req.params.id);
+    if (!author) {
+      return res.status(404).json({ error: 'Author not found' });
+    }
+    res.status(200).json(author);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// POST create a new author
 exports.create = async (req, res) => {
-  //#swagger.tags=["Author"]
+  // #swagger.tags = ["Author"]
+  // #swagger.summary = "Create a new author"
   try {
     const author = new Author(req.body);
     await author.save();
     res.status(201).json(author);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// PUT update an existing author
+exports.update = async (req, res) => {
+  // #swagger.tags = ["Author"]
+  // #swagger.summary = "Update an existing author"
+  // #swagger.parameters['id'] = { in: 'path', description: 'Author ID', required: true, type: 'string' }
+  try {
+    const author = await Author.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!author) {
+      return res.status(404).json({ error: 'Author not found' });
+    }
+    res.status(200).json(author);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// DELETE an author
+exports.remove = async (req, res) => {
+  // #swagger.tags = ["Author"]
+  // #swagger.summary = "Delete an author"
+  // #swagger.parameters['id'] = { in: 'path', description: 'Author ID', required: true, type: 'string' }
+  try {
+    const author = await Author.findByIdAndDelete(req.params.id);
+    if (!author) {
+      return res.status(404).json({ error: 'Author not found' });
+    }
+    res.status(200).json({ message: 'Author deleted' });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -3,21 +3,41 @@ const router = express.Router();
 const controller = require('../controllers/authorController');
 const validate = require('../middleware/validate');
 
-//#swagger.tags = ['Author'] 
-//#swagger.path = '/authors'
 router.get(
     '/', 
+    //#swagger.tags = ['Author']
     //#swagger.summary = 'Get all authors'
     // #swagger.path = '/api/authors'
     controller.getAll);
 
+router.get(
+    '/:id',
+    // #swagger.tags = ['Author']
+    // #swagger.summary = 'Get an author by ID'
+    // #swagger.path = '/api/authors{id}'
+    controller.getOne);
 
-// #swagger.tags = ['Author']
-//#swagger.path = '/authors'
+
 router.post(
     '/', 
+    // #swagger.tags = ['Author']
     //#swagger.summary = 'Post an authors'
     // #swagger.path = '/api/authors'
-    validate.author, controller.create);
+    controller.create);
+
+router.put(
+    '/:id', 
+    // #swagger.tags = ['Author']
+    //#swagger.summary = 'Update an author'
+    // #swagger.path = '/api/authors{id}'
+    validate.author, controller.update);
+
+
+router.delete(
+    '/:id', 
+    // #swagger.tags = ['Author']
+    //#swagger.summary = 'Delete an author'
+    // #swagger.path = '/api/authors{id}'
+    validate.author, controller.remove);
 
 module.exports = router;
