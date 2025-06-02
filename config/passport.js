0cbb58a6e543aa02cdc1ce55254355cad628435f
Arthur Weale <ajayrossy340@gmail.com>
2025-06-02 02:37:31 +0200

    Updated the callback url

--- config/passport.js
+++ config/passport.js
@@ -4,9 +4,9 @@ const GitHubStrategy = require('passport-github2').Strategy;
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: '/auth/github/callback'
+  callbackURL: 'https://cse-w0304-project2.onrender.com/auth/github/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  // You can save user to DB here
+  //Saving user to DB here
   return done(null, profile);
 }));
 
